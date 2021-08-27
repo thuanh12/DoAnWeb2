bed5f27d0107058e29c7399f82fcd3b8d06796db
your name <anhthudinh1220@gmail.com>
2021-08-27 13:41:40 +0700

    update DB env

--- DB/Postgres.js
+++ DB/Postgres.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+
 const Sequelize = require('sequelize');
 const env = process.env.DATABASE_URL;
+
 const sequelize = new Sequelize(env || 'postgres://postgres:daubanh12@localhost:5432/web', {
   define: {
     timestamps: false
